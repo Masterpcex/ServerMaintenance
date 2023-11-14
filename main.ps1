0a6b1473f27969c8c544850c63db048daeb3b9c1
Masterpcex <76758631+Masterpcex@users.noreply.github.com>
2023-11-14 10:41:22 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,6 @@
 $start_time = Get-Date
 
-# Single Instance (no overloads)
+# Ciao Instance (no overloads)
 function MUTEX-CHECK {
     $AppId = "16fcb8bb-e281-472d-a9f6-39f0f32f19f2" # This GUID string is changeable
     $CreatedNew = $false
